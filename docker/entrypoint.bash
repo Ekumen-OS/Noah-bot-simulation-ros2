f010ce54f7218e365a6db93f6b43df3c79f641e8
Lucas Chiesa <lucas.chiesa@gmail.com>
2022-12-19 14:14:18 +0100

    Build dependencies during docker build (#8)
    Signed-off-by: Lucas Chiesa <lucas.chiesa@gmail.com>

---
+++ docker/entrypoint.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+export ROS_DISTRO=humble
+export COLCON_WS=/colcon_ws
+
+# setup ros2 environment
+source "/opt/ros/${ROS_DISTRO}/setup.bash" --
+# source gazebo
+source /usr/share/gazebo/setup.bash --
+# source the colcon workspace
+source "${COLCON_WS}/install/setup.bash" --
+
+exec "$@"
