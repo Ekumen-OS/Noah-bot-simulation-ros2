188cc980be37a521265f949c7a1b73cece1c0c87
Fabricio Emder <elector102@gmail.com>
2023-04-05 13:51:08 -0300

    Addressed Franco comments

--- noah_navigation2/launch/navigation2.launch.py
+++ noah_navigation2/launch/navigation2.launch.py
@@ -1,18 +1,4 @@
-# Copyright 2019 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Author: Darby Lim
+# Copyright 2022 Fabricio Emder
 
 import os
 
@@ -34,7 +20,7 @@ def generate_launch_description():
         default=os.path.join(
             get_package_share_directory('noah_navigation2'),
             'map',
-            'rtabmap.yaml'))
+            'small_house.yaml'))
 
     param_file_name = NOAH_MODEL + '.yaml'
     param_dir = LaunchConfiguration(
