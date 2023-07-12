7ae1dea48d5250b5734944c4962743a21d65431b
IDavGal <ignacio.davila@ekumenlabs.com>
2023-07-11 21:02:14 -0300

    Update Dockerfile to install stable version of webots_ros2
    Signed-off-by: IDavGal <ignacio.davila@ekumenlabs.com>

--- docker/entrypoint.bash
+++ docker/entrypoint.bash
@@ -4,6 +4,9 @@ set -e
 export ROS_DISTRO=humble
 export COLCON_WS=/colcon_ws
 
+echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+echo "source /ros2_ws/install/local_setup.bash" >> ~/.bashrc
+
 # setup ros2 environment
 source "/opt/ros/${ROS_DISTRO}/setup.bash" --
 # source gazebo
